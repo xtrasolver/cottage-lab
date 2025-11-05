3e2fdc1723cdaefee3f82a4fa764ed0c3542ce23
Jaroslav Svoboda (Krtek) <jaroslav.svoboda@elostech.cz>
2025-11-05 22:04:57 +0100

    .

--- init.ps1
+++ init.ps1
@@ -4,13 +4,13 @@ helm repo add argo https://argoproj.github.io/argo-helm
 kubectl create namespace argocd
 helm upgrade --install --namespace  argocd  argocd argo/argo-cd -f tmp\argocd.values.yaml
 kubectl apply -f root.yaml
+kubectl apply -f .\secrets # Repeat
 
 $base64 = kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}"
 [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($base64))
 
 kubectl port-forward service/argocd-server -n argocd 8080:443
 
-kubectl apply -f .\secrets
 
 kubectl apply -f .\infra\auth-acmedns.yaml
 kubectl apply -f .\infra\clusterissuer.yaml
